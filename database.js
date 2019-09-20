be71886cdcc1552fc2da4d2c098d5ce81cfa06fc
juanpablo369 <52577050+juanpablo369@users.noreply.github.com>
2019-09-20 00:00:38 -0500

    db local

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 module.exports = {
-	
   database: {
-    URI: 'mongodb+srv://thordevman:uGrRyuEp8,g$4jK@cluster0-bzo80.mongodb.net/test?retryWrites=true&w=majority'
+    URI: 'mongodb://localhost/yanuatest'
   }
 };
