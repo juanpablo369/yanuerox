377a867a1f539660bc3dcaac06f95050bcabecf5
juanpablo369 <52577050+juanpablo369@users.noreply.github.com>
2019-09-20 00:11:39 -0500

    vista

--- models/persona.js
+++ models/persona.js
@@ -4,11 +4,7 @@ var Schema = mongoose.Schema;
 var PersonaSchema = Schema({
     id: mongoose.Schema.Types.ObjectId,
     external_id: String,
-    apellido: String,
     nombre: String,
-    area: String,
-    carrera: String,
-    foto: String,
     correo: String,
     secretToken: String,
     }, false);
